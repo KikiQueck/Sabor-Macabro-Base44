9b81ba8ad9a17ef334848af28722449f11b92236
TexugoZapz <hugomlgdoge@gmail.com>
2026-06-01 04:11:17 -0300

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,6 @@
 // ====== SABOR MACABRO — CARRINHO ======
 (function () {
-  const WHATSAPP_NUMBER = '5511999999999'; // Troque pelo número real
+  const WHATSAPP_NUMBER = '551196666-6666'; // Troque pelo número real
 
   let cart = [];
 
